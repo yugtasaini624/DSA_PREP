c6ef6fc3fe45d47b008ce837c9ba093fb01248b8
Yugta Saini <yugtasaini@gmail.com>
2026-08-31 21:14:51 +0530

    Day 22
    Added a comment block explaining the Guess Game mechanics.

--- dsa_questions_leetcode75/Binary_Search/guess_no_higher_or_lower.cpp
+++ dsa_questions_leetcode75/Binary_Search/guess_no_higher_or_lower.cpp
@@ -1,10 +1,7 @@
 /*
 We are playing the Guess Game. The game is as follows:
-
 I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
-
 Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
-
 You call a pre-defined API int guess(int num), which returns three possible results:
 
     -1: Your guess is higher than the number I picked (i.e. num > pick).
