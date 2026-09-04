b1abc3ee98bd843a65ef836571b695c5e9c30621
Yugta Saini <yugtasaini@gmail.com>
2026-09-04 21:53:24 +0530

    Day 26

--- top_interview_questions_150/LinkedList/lru_cache.cpp
+++ top_interview_questions_150/LinkedList/lru_cache.cpp
@@ -1,6 +1,5 @@
 /*
 Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
-
 Implement the LRUCache class:
 
     LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
