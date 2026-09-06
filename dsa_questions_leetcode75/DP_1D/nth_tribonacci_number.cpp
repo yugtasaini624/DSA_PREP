9468ae140ab381c974643e3a29eb50dcad164e82
yugtasaini624 <yugtasaini@gmail.com>
2026-09-06 22:09:42 +0530

    Day 28

---
+++ dsa_questions_leetcode75/DP_1D/nth_tribonacci_number.cpp
@@ -0,0 +1,67 @@
+/*
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+
+Example 2:
+
+Input: n = 25
+Output: 1389537
+
+ 
+
+Constraints:
+
+    0 <= n <= 37
+    The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rec(int n, vector<int> &dp){
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = rec(n - 1, dp) + rec(n - 2, dp) + rec(n - 3, dp);
+    }
+
+    int tribonacci(int n) {
+        vector<int> dp(n + 1, -1);
+
+        dp[0] = 0;
+
+        if (n >= 1) dp[1] = 1;
+
+        if (n >= 2) dp[2] = 1;
+
+        if (n >= 3) dp[3] = 2;
+
+        return rec(n, dp);
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Solution obj;
+
+    cout << obj.tribonacci(n);
+
+    return 0;
+}
---
+++ top_interview_questions_150/LinkedList/remove_duplicates_from_sorted_list2.cpp
@@ -0,0 +1,102 @@
+/*
+You are given the head of a sorted linked list.
+
+Delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+
+Example 2:
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 300].
+    -100 <= Node.val <= 100
+    The list is guaranteed to be sorted in ascending order.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *tail = dummy;
+
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode *ptr = head->next;
+        ListNode *prev = head;
+        int val = INT_MAX;
+
+        while(ptr != NULL){
+            if(ptr->val != prev->val){
+                if(prev->val != val){
+                    ListNode *newNode = new ListNode(prev->val);
+                    tail->next = newNode;
+                    tail = tail->next;
+                    prev = ptr;
+                    ptr = ptr->next;
+                } else {
+                    prev = ptr;
+                    ptr = ptr->next;
+                }
+            } else {
+                val = prev->val;
+                prev = ptr;
+                ptr = ptr->next;
+            }
+        }
+        if(prev->next == NULL && prev->val != val){
+            ListNode *newNode = new ListNode(prev->val);
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(1);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(3);
+    head->next->next->next->next->next = new ListNode(4);
+
+    Solution obj;
+
+    ListNode* ans = obj.deleteDuplicates(head);
+    ListNode* temp = ans;
+
+    while (temp != nullptr) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
