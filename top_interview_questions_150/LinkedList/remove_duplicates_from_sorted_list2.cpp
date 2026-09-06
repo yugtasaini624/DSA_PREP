b22c19fe888e58acb0057ee6f16addec8b269eb9
Yugta Saini <yugtasaini@gmail.com>
2026-09-06 22:11:03 +0530

    Day 28

--- top_interview_questions_150/LinkedList/remove_duplicates_from_sorted_list2.cpp
+++ top_interview_questions_150/LinkedList/remove_duplicates_from_sorted_list2.cpp
@@ -1,11 +1,7 @@
 /*
 You are given the head of a sorted linked list.
-
 Delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
-
 Return the linked list sorted as well.
-
- 
 
 Example 1:
 
