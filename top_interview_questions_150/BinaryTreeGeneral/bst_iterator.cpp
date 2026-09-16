b5b65e9434d54fbf4a5ee813c1528b6acf9ef5c8
yugtasaini624 <yugtasaini@gmail.com>
2026-09-16 16:37:50 +0530

    Day 38

---
+++ top_interview_questions_150/BinaryTreeGeneral/bst_iterator.cpp
@@ -0,0 +1,103 @@
+/*
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+    BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+    boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+    int next() Moves the pointer to the right, then returns the number at the pointer.
+
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+Example 1:
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 105].
+    0 <= Node.val <= 106
+    At most 105 calls will be made to hasNext, and next.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+
+    void pushLeft(TreeNode* node) {
+        while(node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+
+    int next() {
+        TreeNode* curr = st.top();
+        st.pop();
+
+        if(curr->right) {
+            pushLeft(curr->right);
+        }
+
+        return curr->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(7);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(15);
+    root->right->left = new TreeNode(9);
+    root->right->right = new TreeNode(20);
+
+    BSTIterator obj(root);
+
+    while(obj.hasNext()) {
+        cout << obj.next() << " ";
+    }
+
+    return 0;
+}
